151132087a0d8fd0b1a2ced6d44d6dcaa7dd2b9d
weianzi2003 <ancool@foxmail.com>
2019-08-02 08:44:53 +0800

    del

--- src/router.js
+++ src/router.js
@@ -5,18 +5,18 @@ Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import(/* webpackChunkName: "home" */ './views/Home')
-    },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: () => import(/* webpackChunkName: "home" */ './views/Home')
+    // },
     {
       path: '/mx',
       name: 'mx',
       component: () => import(/* webpackChunkName: "mx" */ './views/Mx')
     },
     {
-      path: '/workflow',
+      path: '/',
       name: 'workflow',
       component: () => import(/* webpackChunkName: "workflow" */ './views/Workflow')
     },
